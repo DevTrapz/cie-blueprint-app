ddad2105323fca5810c3d01e058ecae9c5fc9484
Andrew Apodaca <andy.apod@gmail.com>
2026-04-01 13:31:25 -0500

    reorders roles and life_purpose/desc text

--- src/components/FillSections.tsx
+++ src/components/FillSections.tsx
@@ -160,8 +160,8 @@ export function FillCenterCenter({ module_core, traits }: CenterCenterType) {
       <div className="center-center-sub-grid-1x2 background-black">
         <div className="flex-col-mid-center background-primary">
           <span className="font-12">{module_core.core_date_range}</span>
-          <h3 className="font-18">{module_core.role}</h3>
-          <strong className="font-12">{module_core.life_purpose}</strong>
+          <h3 className="font-18">{module_core.life_purpose}</h3>
+          <strong className="font-12">{module_core.role}</strong>
         </div>
         <div className="center-center-sub-grid-3x2 background-black">
           <div className="sub-cell-element sub-cell-top-color">
